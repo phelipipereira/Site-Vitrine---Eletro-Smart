30a425c781797dcf606b4fafc1fddf8921924f6e
phelipipereira <phelipimachado8@gmail.com>
2025-06-14 10:57:55 -0300

    Alterações em relação a porta usada pelo servidor

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.get("/api/usuarios", autenticarLider, (req, res) => {
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
